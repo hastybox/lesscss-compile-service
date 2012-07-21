e129fcfd3b22273081e518c4ad8237bc47824315
suicide <psy@get-it.us>
2012-07-21 17:38:08 +0200

    license fixed

--- src/main/java/com/hastybox/lesscss/compileservice/SimpleWebLessCompileService.java
+++ src/main/java/com/hastybox/lesscss/compileservice/SimpleWebLessCompileService.java
@@ -1,4 +1,4 @@
-/* Copyright 2011-2012 The Apache Software Foundation.
+/* Copyright 2011-2012 The Cellador Hastybox Team.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
